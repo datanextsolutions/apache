2b37c43575dba6c1078a4efd881ed89aaca1d735
vagrant <vagrant@localhost.localdomain>
2017-05-02 18:01:00 +0000

    index.html includes node attributes

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,7 +9,13 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<h1> Hello World... </h1>'
+  content "<html>
+           <body>
+             <h1> Hello World... </h1>
+             <h2> HOSTNAME: #{node['hostname']} </h2>
+             <h2> IPADDREESS #{node['ipaddress']} </h2>
+           </body>
+           </html>"
 end
 
 service 'httpd' do
